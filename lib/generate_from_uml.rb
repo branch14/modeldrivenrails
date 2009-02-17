957fb886f8759dfed742e516622c140bc378fca4
phil <phil@gurke.(none)>
2009-02-17 12:38:34 +0100

    name change, some comments, xmi support in progress

--- lib/generate_from_uml.rb
+++ lib/generate_from_uml.rb
@@ -1,4 +1,3 @@
-
 require 'zlib'
 require 'rexml/document'
 
@@ -65,10 +64,12 @@ module UML
             attributes << "#{assoc.klass.name.underscore}_id:integer"
           when '1 -> *', '0..1 -> *'
             options[:associations] << "has_many :#{assoc.klass.name.underscore.pluralize}"
-          #when '* -> *'
-          #  options[:associations] << assoc.through ?
-          #  "has_many :#{assoc.klass.name.underscore.pluralize}, :through => #{assoc.through.underscore.pluralize}" :
-          #    "has_and_belongs_to_many :#{assoc.klass.name.underscore.pluralize}"
+            # honestly, i had some problems with this one, so i left it out for now
+            # and since i literally stopped using * -> * relationsships it really doesn't bother me anymore
+            #when '* -> *'
+            #  options[:associations] << assoc.through ?
+            #  "has_many :#{assoc.klass.name.underscore.pluralize}, :through => #{assoc.through.underscore.pluralize}" :
+            #    "has_and_belongs_to_many :#{assoc.klass.name.underscore.pluralize}"
           end
         end
         args = ['umlified_model', klass.name] + attributes
@@ -85,7 +86,7 @@ module UML
       when '.dia' then load_dia(options)
       when '.xmi' then load_xmi(options)
       else
-        raise "This format '#{ext}' is not supported"
+        raise "The format '#{ext}' is not supported"
       end
     end
 
@@ -95,7 +96,7 @@ module UML
     
     def self.load_dia(options)
       LOG.debug("loading dia with options: #{options}")
-      # dia is gzip'd xml so we'll load it using zlib and rexml
+      # dia is gziped xml so we'll load it using zlib and rexml
       # TODO fallback to ungzip'd attempt of loading the xml
       root = Zlib::GzipReader.open(options[:filename]) { |gz| REXML::Document.new(gz.read).root }
       design = Design.new
@@ -130,54 +131,15 @@ module UML
           handle = conn.attribute(:handle).value.to_i
           connections[handle ^ direction] = to
         end
-        # design.associate(connections[0], connections[1], multiplicities[0], multiplicities[1])
         design.associate(*(connections + multiplicities))
       end
       return design
     end
 
     def self.load_xmi(options)
-      LOG.debug("loading dia with options: #{options}")
-      # dia is gzip'd xml so we'll load it using zlib and rexml
-      # TODO fallback to ungzip'd attempt of loading the xml
-      root = Zlib::GzipReader.open(options[:filename]) { |gz| REXML::Document.new(gz.read).root }
-      design = Design.new
-      # go through the classes of all visible layers
-      xpath = '/XMI/XMI.content/UML:Model/UML:Namespace.ownedElement/UML:Class'
-      REXML::XPath.each(root, xpath) do |xml_klass|
-        oid = xml_klass.attribute('xmi.id').value
-        name = xml_klass.attribute('name').value
-        design << uml_klass = Klass.new(oid, name)
-        # go through the attributes of the class
-	### AB HIER !!! ###
-        xpath = 'child::dia:attribute[@name="attributes"]/dia:composite'
-        REXML::XPath.each(xml_klass, xpath) do |attrib|
-          name = dia_string(attrib, 'name')
-          type = dia_string(attrib, 'type')
-          name, type = name.underscore, type.downcase if options[:force_conventions]
-          uml_klass << Attribute.new(name, type)
-        end
-      end
-      # go through the associations of all visible layers
-      xpath = '/dia:diagram/dia:layer[@visible="true"]/dia:object[@type="UML - Association"]'
-      REXML::XPath.each(root, xpath) do |assoc|
-        direction = REXML::XPath.first(assoc, 'child::dia:attribute[@name="direction"]/dia:enum').text.to_i
-        multiplicities = []
-        xpath = 'child::dia:attribute[@name="ends"]/dia:composite'
-        REXML::XPath.each(assoc, xpath) do |ent|
-          multiplicities << dia_string(ent, 'multiplicity')
-        end
-        connections = []
-        xpath = 'child::dia:connections/dia:connection'
-        REXML::XPath.each(assoc, xpath) do |conn|
-          to = conn.attribute(:to).value
-          handle = conn.attribute(:handle).value.to_i
-          connections[handle ^ direction] = to
-        end
-        # design.associate(connections[0], connections[1], multiplicities[0], multiplicities[1])
-        design.associate(*(connections + multiplicities))
-      end
-      return design
+      LOG.debug("loading xmi with options: #{options}")
+      LOG.debug("Sorry, this implementation is currently in progress.")
+      exit
     end
     
     private
